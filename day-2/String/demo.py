a550736aae649fe3c58526674d93a37909689292
MukeshSahaniCS <sahanigolu8318@gmail.com>
2025-01-22 12:39:20 +0530

    Demo

--- day-2/String/demo.py
+++ day-2/String/demo.py
@@ -10,3 +10,40 @@ print(s2)
 print(s3)
 print(s4)
 
+name = "muKEsh"
+print(name[0]) # m
+print(name[1]) # u
+print(name[-1]) # h
+print(name[:6]) # mukesh
+print(name[0:]) # mukesh
+print(name[0:6:2]) # mks
+
+# function in string
+
+print(len(name)) # 6
+print(name.endswith("esh")) # True
+
+print(name.count("s")) # 1
+print(name.capitalize()) # Mukesh
+
+print(name.lower())
+print(name.upper())
+
+text = "Learning Python is fun"
+print(text.find("Python"))
+print("fun" in text)
+
+# text = text.replace("Python", "Java")
+
+# text.replace("Python", "Java") not change permanently create one string
+
+print(text.replace("Python", "Java"))
+
+
+string = "apple,banana,cherry"
+fruits = string.split(",")
+print(fruits)
+
+joined_text = " - ".join(fruits)
+
+print(joined_text)
